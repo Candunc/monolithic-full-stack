48566bef968f6f39ff188c608a8c19020aff31f4
Duncan Bristow <duncbristow@msn.com>
2019-09-18 12:08:58 -0700

    Untested work on install.sh, removed ubuntu-nginx dep

--- install.sh
+++ install.sh
@@ -1,19 +1,32 @@
 #!/bin/bash
+if [[ $EUID -ne 0 ]]; then
+   echo "This script must be run as root" 
+   exit 1
+fi
 
-#mkdir /tmp/monolithic/
+hide_submodule() {
+	mv ./cache-domains/.git ./cache-domains/.git.bak
+	mkdir ./cache-domains/.git
+}
 
-mkdir ./root
-mkdir ./root/etc
+show_submodule() {
+	rmdir ./cache-domains/.git
+	mv ./cache-domains/.git.bak ./cache-domains/.git
+}
 
-cp -a ./lancache-dns/overlay/etc/bind ./root/etc
+git submodule update --init
 
+# Remove old config files (if they exist)
+rm -r /etc/nginx /etc/bind
+
+## BEGIN lancache-dns
+
+cp -a ./lancache-dns/overlay/etc/bind /etc/
 cp ./lancache-dns/overlay/hooks/entrypoint-pre.d/10_generate_config.sh ./
 sed -i -e 's:/opt/:./:' ./10_generate_config.sh
-sed -i -e 's:/etc/:./root/etc/:' ./10_generate_config.sh
 
 # Bypass a check in 10_generate_config
-mv ./cache-domains/.git ./cache-domains/.git.bak
-mkdir ./cache-domains/.git
+hide_submodule
 
 LANCACHE_IP="10.0.1.1"
 NOFETCH="true"
@@ -22,5 +35,31 @@ USE_GENERIC_CACHE="true"
 
 rm ./10_generate_config.sh
 
-rmdir ./cache-domains/.git
-mv ./cache-domains/.git.bak ./cache-domains/.git
+## BEGIN generic
+
+cp -a ./generic/etc/nginx /etc/
+
+WEBUSER=www-data
+CACHE_MEM_SIZE=500m
+CACHE_DISK_SIZE=500000m
+CACHE_MAX_AGE=3560d
+UPSTREAM_DNS="8.8.8.8 8.8.4.4"
+BEAT_TIME=1h
+LOGFILE_RETENTION=3560
+NGINX_WORKER_PROCESSES=16
+
+. ./generic/overlay/hooks/entrypoint-pre.d/10_setup.sh
+
+ln -s /etc/nginx/sites-available/10_generic.conf /etc/nginx/sites-enabled/10_generic.conf
+
+## BEGIN monolithic
+
+cp -a ./monolithic/etc/nginx /etc/
+
+cp ./monolithic/overlay/hooks/entrypoint-pre.d/15_generate_maps.sh ./
+sed -i -e 's:/data/cachedomains:./cache-domains:' ./15_generate_maps.sh
+
+. ./15_generate_maps.sh
+
+# Return to our initial state
+show_submodule
